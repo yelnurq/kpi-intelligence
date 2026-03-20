5ad3c97dd6826beff30ad850cbb577edaddcc53a
yelnur <elnurzzz@yandex.ru>
2026-03-20 13:19:43 +0500

    Update Dashboard.jsx

--- frontend/src/pages/UserPanel/Dashboard/Dashboard.jsx
+++ frontend/src/pages/UserPanel/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import {
   User, GraduationCap, Briefcase, ShieldCheck, Clock, CheckCircle, 
   AlertCircle, BarChart3, Calendar, ArrowUpRight  
 } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const StatCard = ({ icon: Icon, label, value, trend, colorClass, description, isPrimary }) => (
   <div className={`bg-white p-6 rounded-xl border border-slate-200 shadow-sm relative overflow-hidden group transition-all hover:shadow-md ${isPrimary ? 'ring-1 ring-blue-600/10' : ''}`}>
@@ -163,9 +164,16 @@ const Dashboard = () => {
       <div className="bg-white border border-slate-200 rounded-xl overflow-hidden shadow-sm">
         <div className="px-6 py-4 border-b border-slate-100 bg-slate-50/50 flex justify-between items-center">
           <h3 className="font-bold text-sm text-slate-800 uppercase tracking-tight">Журнал последних достижений</h3>
+          <div className='flex gap-2 items-center'>
+
           <span className="text-[10px] font-bold text-slate-400 uppercase tracking-widest bg-white border border-slate-200 px-3 py-1 rounded-lg shadow-sm">
             Записей: {activities.length}
           </span>
+          <Link to={'/archive'} className="text-[10px] font-bold text-blue-900 uppercase tracking-widest bg-white border border-slate-200 px-3 py-1 rounded-lg shadow-sm pointer hover:text-blue-600">
+              Смотреть все
+          </Link>
+          </div>
+
         </div>
         
         <div className="overflow-x-auto">
@@ -176,7 +184,6 @@ const Dashboard = () => {
                 <th className="px-6 py-4 font-bold text-center">Баллы</th>
                 <th className="px-6 py-4 font-bold text-center">Дата подачи</th>
                 <th className="px-6 py-4 font-bold text-right">Статус верификации</th>
-                <th className="px-6 py-4"></th>
               </tr>
             </thead>
             <tbody className="divide-y divide-slate-50">
@@ -205,11 +212,7 @@ const Dashboard = () => {
                       {item.status === 'approved' ? 'Одобрено' : item.status === 'pending' ? 'На проверке' : 'Отклонено'}
                     </span>
                   </td>
-                  <td className="px-6 py-4 text-right">
-                    <button className="p-2 text-slate-300 hover:text-slate-900 transition-colors rounded-lg">
-                      <ChevronRight size={16} />
-                    </button>
-                  </td>
+                  
                 </tr>
               ))}
             </tbody>

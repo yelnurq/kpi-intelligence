d0453c46fe9ce030c3182084f79a8bf6ea92313c
yelnur <114299301+yelnurq@users.noreply.github.com>
2026-03-29 23:40:09 +0500

    Update Dashboard.jsx

--- frontend/src/pages/UserPanel/Dashboard/Dashboard.jsx
+++ frontend/src/pages/UserPanel/Dashboard/Dashboard.jsx
@@ -182,7 +182,6 @@ const Dashboard = () => {
           KPI Планирование <ArrowUpRight size={14} />
         </Link>
       </div>
-
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-6">
         <div className="lg:col-span-1 space-y-6">
           {loading ? <><StatSkeleton /><StatSkeleton /></> : (

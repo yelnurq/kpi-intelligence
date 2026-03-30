00d866457647039aeca24e5322f09746aac4db91
yelnurq <114299301+yelnurq@users.noreply.github.com>
2026-03-30 11:18:35 +0500

    asd

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -16,35 +16,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-    //     $this->call([
-    //         DepartmentSeeder::class,   
-    //         AcademicSeeder::class,      
-    //         PositionSeeder::class,      
-    //         KpiIndicatorSeeder::class,      
-    //     ]);
+        $this->call([
+            DepartmentSeeder::class,   
+            AcademicSeeder::class,      
+            PositionSeeder::class,      
+            KpiIndicatorSeeder::class,      
+        ]);
 
        
-    //     User::create([
-    //         'name' => 'Yelnur Z',
-    //         'email' => 'test@kpi.test',
-    //         'is_admin' => 0,
-    //         'password' => Hash::make('test@kpi.test'),
-    //         'department_id' => 1,
-    //         'academic_degree_id' => 1,
-    //         'faculty_id' => 1,
-    //         'position_id' => 1,
-    //     ]);
+        User::create([
+            'name' => 'Yelnur Z',
+            'email' => 'test@kpi.test',
+            'is_admin' => 0,
+            'password' => Hash::make('test@kpi.test'),
+            'department_id' => 1,
+            'academic_degree_id' => 1,
+            'faculty_id' => 1,
+            'position_id' => 1,
+        ]);
         
-    //     User::create([
-    //         'name' => 'Yelnur Админ',
-    //         'email' => 'admin@kpi.test',
-    //         'is_admin' => 1,
-    //         'password' => Hash::make('admin@kpi.test'),
-    //         'department_id' => 1,
-    //         'academic_degree_id' => 1,
-    //         'faculty_id' => 1,
-    //         'position_id' => 1,
-    //     ]);
+        User::create([
+            'name' => 'Yelnur Админ',
+            'email' => 'admin@kpi.test',
+            'is_admin' => 1,
+            'password' => Hash::make('admin@kpi.test'),
+            'department_id' => 1,
+            'academic_degree_id' => 1,
+            'faculty_id' => 1,
+            'position_id' => 1,
+        ]);
     
     User::factory(50)->create();
 }
